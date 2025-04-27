46f8cea36f16d5a0f9923b467e991cd206ac0cbb
tainur <tainurrahaman31@gmail.com>
2025-04-27 23:36:05 +0600

    made responsive sectionTitle

---
+++ src/Components/SectionTitle.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SectionTitle = ({ heading, subHeading }) => {
+  return (
+    <div className="w-2/3 md:w-1/2 lg:w-4/12 mx-auto text-center mb-5 md:mb-10 lg:mb-12 mt-8 md:mt-12 lg:mt-24">
+      <p className="text-[16px] lg:text-[20px] text-yellow-500 pb-2 lg:pb-4">
+        ---{subHeading}---
+      </p>
+      <h3 className="text-xl md:text-2xl lg:text-4xl uppercase border-y-2 lg:border-y-4 border-gray-500 py-2 lg:py-4">
+        {heading}
+      </h3>
+    </div>
+  );
+};
+
+export default SectionTitle;
--- src/Pages/Home/Category.jsx
+++ src/Pages/Home/Category.jsx
@@ -10,10 +10,15 @@ import slide2 from "../../assets/home/slide2.jpg";
 import slide3 from "../../assets/home/slide3.jpg";
 import slide4 from "../../assets/home/slide4.jpg";
 import slide5 from "../../assets/home/slide5.jpg";
+import SectionTitle from "../../Components/SectionTitle";
 
 const Category = () => {
   return (
     <section>
+      <SectionTitle
+        heading={"ORDER ONLINE"}
+        subHeading={"From 11:00am to 10:00pm"}
+      ></SectionTitle>
       <Swiper
         slidesPerView={4}
         centeredSlides={true}
@@ -27,31 +32,31 @@ const Category = () => {
       >
         <SwiperSlide>
           <img src={slide1}></img>
-          <h2 className="text-3xl uppercase text-center -mt-16 text-black">
+          <h2 className="text-[14px] lg:text-3xl uppercase text-center -mt-6 lg:-mt-10 text-black">
             Salads
           </h2>
         </SwiperSlide>
         <SwiperSlide>
           <img src={slide2}></img>
-          <h2 className="text-3xl uppercase text-center -mt-16 text-black">
+          <h2 className="text-[14px] lg:text-3xl uppercase text-center -mt-6 lg:-mt-10 text-black">
             Pizzas
           </h2>
         </SwiperSlide>
         <SwiperSlide>
           <img src={slide3}></img>
-          <h2 className="text-3xl uppercase text-center -mt-16 text-black">
+          <h2 className="text-[14px] lg:text-3xl uppercase text-center -mt-6 lg:-mt-10 text-black">
             Soups
           </h2>
         </SwiperSlide>
         <SwiperSlide>
           <img src={slide4}></img>
-          <h2 className="text-3xl uppercase text-center -mt-16 text-black">
+          <h2 className="text-[14px] lg:text-3xl uppercase text-center -mt-6 lg:-mt-10 text-black">
             Desserts
           </h2>
         </SwiperSlide>
         <SwiperSlide>
           <img src={slide5}></img>
-          <h2 className="text-3xl uppercase text-center -mt-16 text-black">
+          <h2 className="text-[14px] lg:text-3xl uppercase text-center -mt-6 lg:-mt-10 text-black">
             Salads
           </h2>
         </SwiperSlide>
